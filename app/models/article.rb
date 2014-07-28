3a01a7e20ba08da1d309ba58edab89043f322448
Rafael Mayor <drmsoul@live.com>
2014-07-27 23:21:52 -0500

    Version 0.1

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+  attr_accessible :text, :title
+  validates :title, presence: true, 
+		length: {minimum: 5}, 
+		uniqueness: true
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2f48a37d426523ec1ec91fb8be18f7a809ca0129ddcef72d5677818b5007979ca604e5747cef3b342864e2dc4b78dfbf13422d2a715978f9aa87fdde4ef5f960'
